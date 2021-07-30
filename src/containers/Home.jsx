3e2e94cb3e405b3770d6b48105a5d08073c43ada
Rmoreno3 <reynaldomoreno173@gmail.com>
2021-07-30 18:44:51 -0400

    Instalando React-router, Creando archivo de rutas e implementando la ruta de Home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -11,7 +11,7 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initalstate';
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
   return (
     <div className='App'>
@@ -40,4 +40,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
