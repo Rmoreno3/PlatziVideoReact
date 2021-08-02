c78f399a7626e3720f2015c3a2037ac9392b3001
Rmoreno3 <reynaldomoreno173@gmail.com>
2021-08-02 11:39:50 -0400

    Creo el Store de la App

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,39 +1,44 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import { connect } from 'react-redux';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import CarrouselItem from '../components/CarrouselItem';
 import Carrousel from '../components/Carrousel';
-import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
-const API = 'http://localhost:3000/initalstate';
-
-const Home = () => {
-  const initialState = useInitialState(API);
+const Home = ({ mylist, trends, originals }) => {
   return (
     <>
       <Search />
-      {initialState.mylist?.length > 0 && (
+      {mylist?.length > 0 && (
         <Categories title='Mi Lista'>
           <Carrousel>
-            {initialState.mylist?.map((item) => <CarrouselItem key={item.id} {...item} />)}
+            {mylist?.map((item) => <CarrouselItem key={item.id} {...item} />)}
           </Carrousel>
         </Categories>
       )}
       <Categories title='Tendencias'>
         <Carrousel>
-          {initialState.trends?.map((item) => <CarrouselItem key={item.id} {...item} />)}
+          {trends?.map((item) => <CarrouselItem key={item.id} {...item} />)}
         </Carrousel>
       </Categories>
 
       <Categories title='Originales de PlatziVideo'>
         <Carrousel>
-          {initialState.originals?.map((item) => <CarrouselItem key={item.id} {...item} />)}
+          {originals?.map((item) => <CarrouselItem key={item.id} {...item} />)}
         </Carrousel>
       </Categories>
     </>
   );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    mylist: state.mylist,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+
+export default connect(mapStateToProps, null)(Home);
